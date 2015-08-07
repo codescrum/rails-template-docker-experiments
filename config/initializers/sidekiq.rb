0f6f041e76d462868040b56b39ac5dd56c5188de
Juan Pablo Amaya <jp.amaya@codescrum.com>
2015-08-06 19:01:08 -0500

    Add kubernetes pods and service descriptors for mongo, redis and webapp

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 require 'sidekiq/api'
-redis_url = Rails.env.development? ? 'redis://127.0.0.1:6379' : 'redis://redis:6379'
+redis_url = Rails.env.development? ? 'redis://127.0.0.1:6379' : "redis://#{ENV['REDIS_SERVICE_HOST']}:#{ENV['REDIS_SERVICE_PORT']}"
 redis_config = { url: redis_url, namespace: 'sidekiq' }
 
 Sidekiq.configure_server do |config|
